a187941dccfcf40029ee016e3aae4b24d8eef45f
Dave200s1 <81190030+Dave200s1@users.noreply.github.com>
2021-11-25 16:27:46 +0100

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,35 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+
+/*QT_BEGIN_NAMESPACE
+namespace Ui { class Calculator; }
+QT_END_NAMESPACE
+*/
+
+namespace Ui {
+    class Calculator;
+}
+
+class Calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Calculator(QWidget *parent = 0); //Konstruktor deklarieren
+    ~Calculator();
+
+private:
+    Ui::Calculator *ui;
+
+
+private slots:
+    void NumPressed();
+    void MathButtonPressed();
+    void EqualButtonPressed();
+    void ChangeNumberSign();
+    void DeleteSolutionPressed();
+
+};
+#endif // CALCULATOR_H
